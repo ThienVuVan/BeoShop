ae76c260bb6ea78976629d1b8864a34f13e3a5ae
ThienVuVan <>
2023-12-23 00:19:13 +0700

    import library and basic config

---
+++ review-service/src/main/java/com/beo/review/ReviewApplication.java
@@ -0,0 +1,11 @@
+package com.beo.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewApplication.class, args);
+    }
+}
